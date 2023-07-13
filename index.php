b292a233ba9bd15d3f17f4c3b9af4c7e26e13083
Eligio Biasi <ex_98@live.it>
2023-07-13 16:19:01 +0200

    added first object

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
-<!-- è definita una classe ‘Movie’ :
- => all'interno della classe sono dichiarate delle variabili d'istanza
- => all'interno della classe è definito un costruttore
- => all'interno della classe è definito almeno un metodo
-vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+<!-- definiamo una classe ‘Movie’ : -->
+
+<?php
+
+// => all'interno della classe sono dichiarate delle variabili d'istanza 
+
+ class Movie {
+    public $title;
+    public $genre;
+    public $duration;
+
+    // => all'interno della classe è definito un costruttore
+
+    function __construct(string $title, string $genre, int $duration){
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->duration = $duration;
+    }
+
+    // => all'interno della classe è definito almeno un metodo
+        
+        function getTitle(){
+            return $this->title;
+        }
+ }
+
+
+
+    $western = new Movie('il Buono, il Brutto e il Cattivo', 'spaghetti Western', 180);
+    var_dump($western)
+ ?>
